d24a9ccee1a2b03615dde87bab79b76d823d8fac
Dashaarth <artdarya33@gmail.com>
2023-10-05 15:53:39 +0300

    Initial Commit

---
+++ MyFirstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstProject
+//
+//  Created by Dasha Masiuk on 5.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
